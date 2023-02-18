b6cfabe16f6b97aeb091f0f7e575c40b361386de
KDissanayke-Bootcamp <ssndkavindra@gmail.com>
2023-02-19 02:17:37 +0530

    p3

--- src/Example.java
+++ src/Example.java
@@ -1,14 +1,12 @@
 class Example{
-    public static void myMethod(int i){
-        System.out.println("myMethod(int)");
+    public static void myMethod(int i, double d){
+        System.out.println("myMethod(int,double)");
     }
-    public static void myMethod(double d){
-        System.out.println("myMethod(double)");
+    public static void myMethod(double d, int i){
+        System.out.println("myMethod(double,int)");
     }
     public static void main(String args[]){
-        myMethod(111);
-        myMethod(1.5);
-        System.out.println();
+        myMethod(111,1.5);
+        myMethod(1.5,111);
     }
 }
-
